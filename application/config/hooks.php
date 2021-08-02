c9baa9d523a1bf44c3c071a53a7e01494470a59d
Anjali-argusoft <agajjar@argusoft.com>
2021-08-02 17:54:32 +0530

    hooks example practised

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,13 +1,33 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| Hooks
-| -------------------------------------------------------------------------
-| This file lets you define "hooks" to extend CI without hacking the core
-| files.  Please see the user guide for info:
-|
-|	https://codeigniter.com/user_guide/general/hooks.html
-|
-*/
+$hook['pre_controller'][] = array(
+    'class'    => 'About',
+    'function' => 'displaymessage',
+    'filename' => 'About.php',
+    'filepath' => 'hooks',
+    'params'   => array('beer', 'wine', 'snacks')
+);
+
+$hook['pre_controller'][] = array(
+    'class'    => 'About',
+    'function' => 'displaymessage',
+    'filename' => 'About.php',
+    'filepath' => 'hooks',
+    'params'   => array('coke', 'chips', 'icecream')
+);
+
+$hook['post_controller'] = array(
+    'class'    => 'About',
+    'function' => 'displaymessage1',
+    'filename' => 'About.php',
+    'filepath' => 'hooks',
+);
+
+$hook['display_override'][] = array(
+    'class' => 'Minifyhtml',
+    'function' => 'output',
+    'filename' => 'Minifyhtml.php',
+    'filepath' => 'hooks',
+    'params' => array()
+);
